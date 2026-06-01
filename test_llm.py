010d28052384a6dca737929ac6e1ca69f0e276f9
swasti ranjan <swasti.ranjan21@gmail.com>
2026-06-02 00:54:07 +0530

    day 1: setup venv, install genai SDK, test LLM call with dotenv

---
+++ test_llm.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+load_dotenv()
+api_key = os.getenv("GEMINI_API_KEY")
+client = genai.Client(api_key=api_key)
+response = client.models.generate_content(
+    model="gemini-2.5-flash",
+    contents="explain what a binary tree is in 2 lines"
+)
+print(response.text)
